5dc75c2ec59460006b100abe9e91c43647f91b8c
Neobyte01 <neobyte01@hotmail.com>
2023-05-01 23:03:22 +0200

    Built specific work page for personal website project

--- src/routes/work/Website.svelte
+++ src/routes/work/Website.svelte
@@ -1,1 +1,83 @@
-EmilWagman.com
+<script>
+  import { t } from "../../i18n";
+</script>
+<div class="content">
+    <h2>EmilWagman.com</h2>
+
+    <div class="images">
+        <img src="./website/image_2.png" style="transform: rotate(-1deg);">
+        <img src="./website/image_1.png" style="transform: rotate(0.5deg);">
+    </div>
+
+    <p>{@html $t("work.website.article")}</p>
+    <br/>
+    
+    <a class="github" href="https://github.com/Neobyte01/neobyte01.github.io" target="_blank">
+        <img src="./github.png" />    
+        <span>Github repository</span>
+    </a>
+</div>
+
+<style>
+    .content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        margin-bottom: 140px;
+    }
+
+    h2 {
+        margin-top: 96px;
+        margin-bottom: 70px;
+
+        font-size: 64px;
+        font-family: "Alice", sans-serif;
+        text-align: center;
+    }
+
+    .images {
+        margin-bottom: 42px;
+    }
+
+    .images > img:first-child {
+        margin-right: 28px;
+        margin-bottom: 78px;
+    }
+
+    .images > img:last-child {
+        margin-top: 78px;
+    }
+
+    .images > img {
+        width: 400px;
+
+        border-radius: 1px;
+        border: 1px solid white;
+    }
+
+    p {
+        margin-bottom: 18px;
+        width: 690px;
+
+        font-size: 18px;
+        line-height: 30px;
+    }
+
+    a {
+        display: block;
+        width: 690px;
+        font-size: 18px;
+    }
+
+    .github {
+        display: flex;
+        align-items: center;
+    }
+
+    .github > img {
+        width: 42px;
+        margin-right: 12px;
+    }
+
+</style>
--- src/translations.js
+++ src/translations.js
@@ -16,6 +16,17 @@ export default {
         "work.built": "Built with",
 
         "work.website.description": "Personal website meant to market myself to people and organizations.", 
+        "work.website.article": `
+            When I built this website my goal was primarily to have a place where I can show off projects I’ve worked on, to future employers, but I realized quickly that it could be a lot more. Add a little information about myself and my life outside of the professional and it all of a sudden becomes a gathering place for myself, a place where I can market myself to both people and organizations.
+            <br></br>
+            When it came to starting the project I began with designing the website in the design program Figma, a popular tool for creating interactable prototypes and wireframes for webbsites, apps and the like. Using a design program let me easily experiment with different layouts, texts, fonts and more.
+            <br></br>
+            With a designprototype I could start building the website for real, something I decided to do with the Javascript framework Svelte which is an absolute pleasure to work in, with its simple syntax and support for reusable components. A perfect choice for a smaller and not that complex website such as this.
+            <br></br>
+            I went from design to code and back again in iterations in order to build an experience I could be proud of. When I was happy with it I used Github pages to host the website and One.com to get the domain I wanted.
+            <br></br>
+            It was a really fun project that let me do much more of what I enjoy than just programming software, and something that I will continue to develop and update over time.
+        `,
 
         "nav.cv": "Resumé",
         "nav.about": "About",
@@ -40,6 +51,17 @@ export default {
         "work.built": "Byggt med",
 
         "work.website.description": "Personlig webbsida med syftet att markandsföra mig själv till människor och organisationer.", 
+        "work.website.article": `
+            När jag byggde den här webbsidan var mitt mål framförallt att ha ett ställe där jag kan visa upp projekt jag har jobbat på för framtida anställar men jag insåg snabbt att den kunde vara mycket mer än det. Lägg till lite information om mig själv och mitt liv utanför det professionella och helt plötsligt hade jag en samlingsplats för mig själv, en plats för att marknadsföra mig själv till både människor och organisationer.
+            <br></br>
+            När det kom till att börja projektet startade jag med att designa webbsidan i designprogrammet Figma, ett populärt verktyg för att skapa interagerbara prototyper och trådramar till webbsidor, appar och liknande. Att använda ett designprogram lät mig enkelt experimentera med olika layouts, texter, typsnitt och mera.
+            <br></br>
+            Med hjälp av en designprototyp kunde jag börja bygga webbsidan på riktigt, något jag valde att göra i Javascript-ramverket Svelte som är ett absolut nöje att arbeta i med sin enkla syntax och stöd för återanvändbara komponenter. Ett perfekt val för en liten och inte särskilt komplex så som denna.
+            <br></br>
+            jag gick från design till kod och sedan tillbaka igen i iterationer så att jag kunde bygga en upplevelse jag var nöjd med. När jag väl var det så använde jag Github pages för att hosta webbsidan och One.com för att få domänen jag ville ha.
+            <br></br>
+            Det var ett riktigt kul projekt som lät mig göra mycket mer jag gillar att göra än att bara programmera mjukvara, och något som jag fortsätter att uveckla och uppdatera över tid.
+        `,
 
         "nav.cv": "Mitt cv",
         "nav.about": "Om mig",
